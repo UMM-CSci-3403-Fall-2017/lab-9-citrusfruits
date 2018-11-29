c6bb809c824a616a4c114bde0d41338bdd645ebb
Mark Schauer <marksthe1@gmail.com>
2018-11-29 11:21:38 -0600

    Fix lines of code and add to while loop

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -1,29 +1,43 @@
 package segmentedfilesystem;
 
+import java.io.IOException;
 import java.net.*;
 import java.util.ArrayList;
 
 public class Main {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         int port = 6014;
         ArrayList<byte[]> header = new ArrayList<>();
         ArrayList<byte[]> data = new ArrayList<>();
 
         try {
             byte[] buffer = new byte[1028];
-            DatagramPacket packet = new DatagramPacket(buffer, 0);
             InetAddress host = InetAddress.getByName("heartofgold.morris.umn.edu");
+            DatagramPacket packet = new DatagramPacket(buffer, 0, host, port);
             DatagramSocket socket = new DatagramSocket(port);
 
+            socket.send(packet);
+
             while (true) {
-                if (buffer[0] == 0){
-                        header.add(buffer);
-                }
-                else {
-                    data.add(buffer);
-                }
+                DatagramPacket received = new DatagramPacket(buffer, buffer.length);
+                socket.receive(received);
+                byte[] rdata = received.getData();
+                int length = received.getLength();
+                System.out.println(rdata);
+                System.out.println(length);
             }
+
+
+
+//            while (true) {
+//                if (buffer[0] == 0){
+//                        header.add(buffer);
+//                }
+//                else {
+//                    data.add(buffer);
+//                }
+//            }
 
 
 
@@ -32,8 +46,6 @@ public class Main {
         } catch (SocketException e) {
             e.printStackTrace();
         }
-
-        //DatagramSocket socket = new DatagramSocket(6014);
     }
 
 }
